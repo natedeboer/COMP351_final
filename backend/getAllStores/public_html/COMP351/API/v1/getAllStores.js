2909cf63c61458ac6106744ce8803249694711c0
natedeboer <64807683+natedeboer@users.noreply.github.com>
2021-04-26 21:53:18 -0700

    Add files via upload

---
+++ backend/getAllStores/public_html/COMP351/API/v1/getAllStores.js
@@ -0,0 +1,38 @@
+let http = require('http');
+const mysql = require("mysql");
+
+const db = mysql.createPool({
+    connectionLimit: 10,
+    multipleStatements: true,
+    host: "localhost",
+    user: "crazymup_final_project",
+    password: "final_project",
+    database: "crazymup_final_project"
+});
+
+
+var server = http.createServer(function(req, res) {
+    res.writeHead(200, { 
+        'Content-Type': 'application/json',
+        'Access-Control-Allow-Origin': 'https://thomasbotelho.com'
+    });
+    db.getConnection(function (err, connection) {
+        if (err) {
+            throw err;
+        }
+        
+        let apiSQL = "UPDATE adminPage SET getstore = getStore + 1";
+            connection.query(apiSQL, function (err, result) {
+                    if (err) throw err;
+                });
+        
+        let sql = "SELECT * FROM store";
+        db.query(sql, function (err, result) {
+            if (err) throw err;
+            console.log(result);
+            res.end(JSON.stringify(result));
+        })
+    })
+
+});
+server.listen();
