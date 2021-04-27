57f355509af352695c22780d8da02c6d33ee1613
natedeboer <64807683+natedeboer@users.noreply.github.com>
2021-04-26 21:48:48 -0700

    Add files via upload

---
+++ createItem/public_html/COMP351/API/v1/createItem.js
@@ -0,0 +1,48 @@
+let http = require('http');
+const mysql = require("mysql");
+
+const db = mysql.createPool({
+    connectionLimit: 10,
+    multipleStatements: true,
+    host: "localhost",
+    user: "crazymup_final_project",
+    password: "final_project",
+    database: "crazymup_final_project"
+});
+
+const server = http.createServer(function(request, response) {
+    var body = '';
+    request.on('data', function(data) {
+      body += data;
+    });
+    request.on('end', function() {
+      console.log('Body: ' + body);
+      response.writeHead(200, {
+          'Access-Control-Allow-Origin': 'https://thomasbotelho.com',
+          'Access-Control-Allow-Headers': "Origin, X-Requested-With, Content-Type, Accept",
+          'Content-Type': 'application/json'
+      });
+      response.end(body);
+      
+      let jsonParse = JSON.parse(body);
+      
+        db.getConnection(function (err, connection) {
+            if (err) {
+                throw err;
+            }
+            
+            let SQL = "UPDATE adminPage SET postItem = postItem + 1";
+            connection.query(SQL, function (err, result) {
+                    if (err) throw err;
+                    response.end(result);
+                });
+            
+            connection.query("INSERT INTO item ( itemName,itemDesc,itemPrice) VALUES (?,?,?)", [jsonParse[0].itemName, jsonParse[0].itemDesc, jsonParse[0].itemPrice], function (err, result) {
+                if (err) throw err;
+                response.end(result);
+                });
+            connection.release();
+        });
+    });
+});
+server.listen();
